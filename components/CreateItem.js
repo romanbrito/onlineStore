63dc4fe8fb5d7d8b73cb07b766104327b540751d
Roman Brito <romanbrito1@gmail.com>
2018-12-14 10:18:57 -0600

    initial queries and mutations with apollo

---
+++ components/CreateItem.js
@@ -0,0 +1,112 @@
+import React, {Component} from 'react';
+import {Mutation} from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+import ErrorMessage from './ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+    mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+    ) {
+        createItem(
+            title: $title
+            description: $description
+            price: $price
+            image: $image
+            largeImage: $largeImage
+        ) {
+            id
+        }
+    }
+`;
+
+class CreateItem extends Component {
+  state = {
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
+  };
+
+  // function for input change
+  handleChange = event => {
+    const {name, type, value} = event.target;
+    const val = type === 'number' ? parseFloat(value) : value;
+    this.setState({[name]: val})
+  };
+
+  render() {
+    return (
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {/*(mutationfunction, payload) => {}*/}
+        {(createItem, {loading, error, called}) => (
+          <Form
+            onSubmit={async event => {
+              // stop form from submiting
+              event.preventDefault();
+              // call the mutation
+              const res = await createItem();
+              // change them to the single item page
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id}
+              });
+            }}
+          >
+            <ErrorMessage error={error}/>
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+CreateItem.propTypes = {};
+
+export default CreateItem;
+export {CREATE_ITEM_MUTATION};
