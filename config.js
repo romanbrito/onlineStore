60a57af86ab9f4a846f8c95f3fc524e02618f77d
Roman Brito <romanbrito1@gmail.com>
2018-12-14 02:30:07 -0600

    adding apollo client to query the backend server

---
+++ config.js
@@ -0,0 +1,4 @@
+// This is client side config only - don't put anything in here that shouldn't be public!
+export const endpoint = `http://localhost:60000/simple/v1/cjplv078700040114rjd2e1he`;
+export const prodEndpoint = `http://localhost:60000/simple/v1/cjplv078700040114rjd2e1he`;
+export const perPage = 4;
