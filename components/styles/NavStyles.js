f5766998155b6f95b68d09638ce225353e79ed5a
Roman Brito <romanbrito1@gmail.com>
2018-12-19 12:14:03 -0600

    sign out button component with query

--- components/styles/NavStyles.js
+++ components/styles/NavStyles.js
@@ -18,6 +18,9 @@ const NavStyles = styled.ul`
     background: none;
     border: 0;
     cursor: pointer;
+    color: ${props => props.theme.black};
+    font-weight: 800;
+    font-family: 'Lato', sans-serif;
     @media (max-width: 700px) {
       font-size: 10px;
       padding: 0 10px;
