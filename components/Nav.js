10d19cbd89bc7493af414466aa9435ddc11e819e
Roman Brito <romanbrito1@gmail.com>
2018-12-19 11:12:17 -0600

    showing certain nav elements when signedin and not signedin

--- components/Nav.js
+++ components/Nav.js
@@ -4,30 +4,38 @@ import User from './User';
 
 const Nav = () => {
   return (
-    <NavStyles>
-      <User>
-        {({data: {me}}) => {
-          console.log(me);
-          if (me) return <p>{me.name}</p>;
-          return null;
-        }}
-      </User>
-      <Link href="/items">
-        <a>Shop</a>
-      </Link>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
-      <Link href="/signup">
-        <a>Signup</a>
-      </Link>
-      <Link href="/orders">
-        <a>Orders</a>
-      </Link>
-      <Link href="/me">
-        <a>Account</a>
-      </Link>
-    </NavStyles>
+
+    <User>
+      {({data: {me}}) => (
+        <NavStyles>
+          <Link href="/items">
+            <a>Shop</a>
+          </Link>
+          {me && (
+            // react fragment
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+            </>
+            // end react fragment
+          )}
+          {!me && (
+            <Link href="/signup">
+              <a>Sign In</a>
+            </Link>
+          )}
+
+        </NavStyles>
+      )}
+    </User>
+
   );
 };
 
