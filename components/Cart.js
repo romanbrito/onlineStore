c14a2b93bce66ed0766a21c655320c27efe9b52c
Roman Brito <romanbrito1@gmail.com>
2018-12-21 20:09:18 -0600

    initial react cart

---
+++ components/Cart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
+import SickButton from './styles/SickButton';
+
+
+const Cart = () => {
+  return (
+    <CartStyles open={true}>
+      <header>
+        <CloseButton title="close">&times;</CloseButton>
+        <Supreme>Your Cart</Supreme>
+        <p>You Have ___ Items in your cart.</p>
+      </header>
+
+      <footer>
+        <p>$10.10</p>
+        <SickButton>Checkout</SickButton>
+      </footer>
+    </CartStyles>
+  )
+};
+
+export default Cart;
