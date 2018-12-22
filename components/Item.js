4b0d3a9c73cedb6891ed790dae4a5e0fff1fa10e
Roman Brito <romanbrito1@gmail.com>
2018-12-22 11:12:24 -0600

    connecting to the backend add to cart

--- components/Item.js
+++ components/Item.js
@@ -6,6 +6,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag'
 import formatMoney from '../lib/formatMoney';
 import DeleteItem from './DeleteItem';
+import AddToCart from './AddToCart';
 
 class Item extends Component {
   static propTypes = {
@@ -34,7 +35,7 @@ class Item extends Component {
             query: {id: item.id}
           }}><a>Edit </a>
           </Link>
-          <button>Add to Cart</button>
+          <AddToCart id={item.id}/>
           <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
