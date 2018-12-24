140fb87530c86a30eb7c0f9e17b664b900afee0f
Roman Brito <romanbrito1@gmail.com>
2018-12-24 12:31:16 -0600

    dealing with delete items in cart items

--- components/DeleteItem.js
+++ components/DeleteItem.js
@@ -1,35 +1,35 @@
-import React, {Component} from 'react';
-import {Mutation} from 'react-apollo';
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import {ALL_ITEMS_QUERY} from './Items';
+import { ALL_ITEMS_QUERY } from './Items';
 
 const DELETE_ITEM_MUTATION = gql`
-  mutation DELETE_ITEM_MUTATION($id: ID!) {
-      deleteItem(id: $id) {
-          id
-      }
-  }
+    mutation DELETE_ITEM_MUTATION($id: ID!) {
+        deleteItem(id: $id) {
+            id
+        }
+    }
 `;
 
 class DeleteItem extends Component {
   update = (cache, payload) => {
-    // Manually update the cache on the client, so it matches the server
+    // manually update the cache on the client, so it matches the server
     // 1. Read the cache for the items we want
-    const data = cache.readQuery({query: ALL_ITEMS_QUERY});
-    // 2. Filter the deleted cache out of the page
-    data.allItems = data.allItems.filter(item => item.id !== payload.data.deleteItem.id );
-    // 3. Put the items back
-    cache.writeQuery({query: ALL_ITEMS_QUERY, data});
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log(data, payload);
+    // 2. Filter the deleted itemout of the page
+    data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
+    // 3. Put the items back!
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
   };
-
   render() {
     return (
       <Mutation
         mutation={DELETE_ITEM_MUTATION}
-        variables={{id: this.props.id}}
+        variables={{ id: this.props.id }}
         update={this.update}
       >
-        {(deleteItem, {error}) => (
+        {(deleteItem, { error }) => (
           <button
             onClick={() => {
               if (confirm('Are you sure you want to delete this item?')) {
@@ -42,7 +42,6 @@ class DeleteItem extends Component {
             {this.props.children}
           </button>
         )}
-
       </Mutation>
     );
   }
