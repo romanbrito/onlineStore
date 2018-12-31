a479513eb16a95b3a68baa66261ef23ade5ee0e0
Roman Brito <romanbrito1@gmail.com>
2018-12-31 17:06:44 -0600

    examples of how to use jest

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -1,6 +1,21 @@
 describe('sample test 101', () => {
-  console.log('I am running some tests');
-  // test('sample test 101', () => {
-  //expect(myBeverage.sour).toBeFalsy();
-  // });
+// it is the same as test
+  // first test
+  it('works as expected', () => {
+    const age = 100;
+    expect(1).toEqual(1);
+    expect(age).toEqual(100);
+  });
+  // second test
+  it('handles ranges just fine', () => {
+    const age = 200;
+    expect(age).toBeGreaterThan(100);
+  });
+  // third test
+  // it.only or fit, it.skip or xit to only do test or skip
+  it('makes a list of dog names', () => {
+    const dogs = ['snickers', 'hugo'];
+    expect(dogs).toEqual(dogs);
+    expect(dogs).toContain('snickers');
+  })
 });
